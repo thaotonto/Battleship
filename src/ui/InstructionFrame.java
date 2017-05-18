9d3f51be97bf29e4c5c066de84bda92fb184750f
Doan The Long <thelong0705@gmail.com>
2017-05-18 08:33:38 +0700

    added instruction

--- src/ui/InstructionFrame.java
+++ src/ui/InstructionFrame.java
@@ -1,7 +1,11 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Created by tonto on 5/10/2017.
@@ -9,12 +13,47 @@ import java.awt.*;
 public class InstructionFrame extends JFrame {
     private int INTRUCTION_WIDTH = 500;
     private int INTRUCTION_HEIGHT = 500;
-
+    private JTextArea jTextArea;
+    private JScrollPane jScrollPane;
+    private JPanel instructionpanel;
     public InstructionFrame() throws HeadlessException {
         setSize(INTRUCTION_WIDTH, INTRUCTION_HEIGHT);
         setAlwaysOnTop(true);
         setTitle("INSTRUCTION");
         setVisible(true);
+        setLayout(new BorderLayout());
         setLocationRelativeTo(null);
+        instructionpanel= new JPanel();
+        setUpPanel();
+    }
+
+    private void setUpPanel() {
+        instructionpanel.setLayout(new BorderLayout());
+        this.add(instructionpanel,BorderLayout.CENTER);
+        jTextArea = new JTextArea();
+        jTextArea.setLineWrap(true);
+        jTextArea.setWrapStyleWord(true);
+        jTextArea.setEditable(false);
+        jTextArea.setFont(new Font("CONSOLAS", Font.PLAIN, 15));
+        jTextArea.setBackground(Color.WHITE);
+        jTextArea.setBorder(BorderFactory.createCompoundBorder(
+                jTextArea.getBorder(),
+                BorderFactory.createEmptyBorder(5, 10, 5, 5)));
+        jScrollPane = new JScrollPane(jTextArea);
+        jScrollPane.setBackground(Color.WHITE);
+        jScrollPane.setBorder(BorderFactory.createMatteBorder(1,0,0,0,Color.BLUE));
+        File instruction = new File("resources/instruction.txt");
+        try {
+            Scanner scanner = new Scanner(instruction);
+            while(scanner.hasNextLine()){
+                jTextArea.append("\n");
+                jTextArea.append(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        jTextArea.setCaretPosition(0);
+
+        add(jScrollPane);
     }
 }
